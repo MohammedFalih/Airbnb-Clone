3e9b8d008a05ae87fe7265068649690247a8e14c
MohammedFalih <yunusfalih123@gmail.com>
2024-07-30 21:12:13 +0530

    installed-devise

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from 'el-transition'
+
+export default class extends Controller {
+  static targets = ['dropDown', 'openUserMenu']
+
+  connect() {
+    this.openUserMenuTarget.addEventListener("click", (e) => {
+      openDropdown(this.dropDownTarget)
+    })
+  }
+}
+
+function openDropdown(element) {
+  toggle(element)
+}
+
+// function closeDropdown() {
+//   leave(this.dropdownTarget).then(() => {
+//     element.destroy();
+//   })
+// }
