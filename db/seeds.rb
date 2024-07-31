092387d9284bd663d8a56202254379e09bb6a6ae
MohammedFalih <yunusfalih123@gmail.com>
2024-07-31 15:53:37 +0530

    design:propertylists

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.find_or_create_by!({
+    name: 'Hotel Taj',
+    headline: 'Sample',
+    description: 'Luxurious',
+    address_1: 'Coastal road',
+    city: 'Mumbai',
+    state: 'Maharastra',
+    country: 'India'
+})
+Property.find_or_create_by!({
+    name: 'Hilton',
+    headline: 'Sample',
+    description: 'Luxurious',
+    address_1: 'Guindy',
+    city: 'Chennai',
+    state: 'Tamilnadu',
+    country: 'India'
+})
+Property.find_or_create_by!({
+    name: 'Burj khalifa',
+    headline: 'Sample',
+    description: 'Luxurious',
+    address_1: 'Rashed',
+    city: 'Downtown Dubai',
+    state: 'Dubai',
+    country: 'UAE'
+})
