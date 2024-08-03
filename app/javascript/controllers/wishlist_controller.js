bdda8dc0c2f41949f78d36f0626c23864d997f8c
MohammedFalih <yunusfalih123@gmail.com>
2024-08-03 22:13:01 +0530

    implement wishlist with login check

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -2,12 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     updateWishlist() {
+        const userLoggedIn = this.element.dataset.userLoggedIn;
+
+        if (userLoggedIn === "false") {
+            document.querySelector(".js-login").click();
+            return;
+        }
+
         if (this.element.dataset.status === "false") {
-            this.element.style.fill = "#FF5A5F";
+            this.element.style.fill = "#FF5A5F"; 
             this.element.dataset.status = "true";
         } else {
             this.element.style.fill = "none";
-            this.element.dataset.status = "false"
+            this.element.dataset.status = "false";
         }
     }
 }
