b09df68390128720eac52769a246915842b93536
MohammedFalih <yunusfalih123@gmail.com>
2024-08-04 00:27:14 +0530

    adding to wishlish

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # Defines the root path route ("/")
   root "home#index"
 
-
+  namespace :api do
+    resources :wishlists, only: [:create, :destroy]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
