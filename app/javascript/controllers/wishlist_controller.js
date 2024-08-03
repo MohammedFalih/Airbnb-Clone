26c076d201cf5260958236831dd68ce291f25af2
MohammedFalih <yunusfalih123@gmail.com>
2024-08-03 20:08:29 +0530

    designed wishlist icon for properties

---
+++ app/javascript/controllers/wishlist_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    updateWishlist() {
+        if (this.element.dataset.status === "false") {
+            this.element.style.fill = "#FF5A5F";
+            this.element.dataset.status = "true";
+        } else {
+            this.element.style.fill = "none";
+            this.element.dataset.status = "false"
+        }
+    }
+}
