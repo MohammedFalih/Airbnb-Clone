beeaedd752853ac190b2e24186f5c4ef33606019
MohammedFalih <yunusfalih123@gmail.com>
2024-08-08 20:37:27 +0530

    fixed/errors-with-signups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 require "open-uri"
-
 description = <<-DESCRIPTION
 <div>Explore the nature and art oasis at our unique property. The living room, a cozy masterpiece, and the fully equipped kitchen are ideal for cooking and entertaining. Step outside to our garden patio, unwind, and enjoy morning birdsong. Tastefully decorated bedrooms, a powder room, and utility area complete the experience.<br />Note: The property is surrounded by a residential area. Despite initial surroundings, I am sure that, stepping in will fill your mood with joy and happiness.
 </div>
@@ -8,18 +7,6 @@ description = <<-DESCRIPTION
 <h4 class="font-medium" tabindex="-1">Guest Policy</h4>
 <p>Entire Property is yours!! Wish you fun and happy stay!!</p>
 DESCRIPTION
-
-# amenity1 = Amenity.create!(name: "Kitchen")
-# amenity1.icon.attach(io: File.open("app/assets/images/amenity_icons/kitchen.svg"), filename: amenity1.name)
-
-# amenity2 = Amenity.create!(name: "Private pool")
-# amenity2.icon.attach(io: File.open("app/assets/images/amenity_icons/private_pool.svg"), filename: amenity2.name)
-
-# amenity3 = Amenity.create!(name: "Wifi")
-# amenity3.icon.attach(io: File.open("app/assets/images/amenity_icons/wifi.svg"), filename: amenity3.name)
-
-# amenity4 = Amenity.create!(name: "Esssentials", description:"Towels, bed sheets, soap and toilet paper")
-# amenity4.icon.attach(io: File.open("app/assets/images/amenity_icons/essentials.svg"), filename: amenity4.name)
 
 amenities_data = [
   { name: "Air conditioning", icon: "air_conditioning.svg" },
@@ -60,7 +47,7 @@ amenities_data = [
   { name: "Smoke alarm", icon: "smoke_alarm.svg" },
   { name: "TV", icon: "tv.svg" },
   { name: "Washing machine", icon: "washing_machine.svg" },
-  { name: "Wifi", icon: "wifi.svg" }
+  { name: "Wifi", icon: "wifi.svg" },
 ]
 
 amenities_data.each do |data|
@@ -69,35 +56,29 @@ end
 
 pictures = []
 20.times do
-  pictures << URI.open(Faker::LoremFlickr.image)
+  pictures << URI.parse(Faker::LoremFlickr.image).open
 end
 
 user = User.create!({
   email: "test1@gmail.com",
   password: "123456",
-  name: Faker::Lorem.unique.sentence(word_count: 3),
-  address_1: Faker::Address.street_address,
-  address_2: Faker::Address.street_name,
-  city: Faker::Address.city,
-  state: Faker::Address.state,
-  country: Faker::Address.country
 })
 
-user.picture.attach(io: pictures[0], filename: user.name)
+# Create the profile for the first user
+profile = user.profile # Get the profile for the user
+profile.picture.attach(io: pictures[0], filename: "#{profile.name}.jpg") if profile
 
 19.times do |i|
-  random_user = User.create!({
-    email: "test#{i + 2}@gmail.com",
-    password: "123456",
-    name: Faker::Lorem.unique.sentence(word_count: 3),
-    address_1: Faker::Address.street_address,
-    address_2: Faker::Address.street_name,
-    city: Faker::Address.city,
-    state: Faker::Address.state,
-    country: Faker::Address.country
-  })
+  random_user = User.create!(
+    {
+      email: "test#{i + 2}@gmail.com",
+      password: "123456",
+    }
+  )
 
-  random_user.picture.attach(io: pictures[i + 1], filename: user.name)
+  # Create a profile for each new user
+  random_profile = random_user.profile
+  random_profile.picture.attach(io: pictures[i + 1], filename: "#{random_profile.name}.jpg") if random_profile
 end
 
 6.times do |i|
@@ -114,16 +95,16 @@ end
     bedroom_count: (2..5).to_a.sample,
     bed_count: (4..10).to_a.sample,
     guest_count: (4..20).to_a.sample,
-    bathroom_count: (1..4).to_a.sample
+    bathroom_count: (1..4).to_a.sample,
   })
 
-  property.images.attach(io: File.open("db/images/property_#{i + 1}.png"), filename: "property_#{i + 1}.png")
-  property.images.attach(io: File.open("db/images/property_7.png"), filename: "property_7.png")
-  property.images.attach(io: File.open("db/images/property_8.png"), filename: "property_8.png")
-  property.images.attach(io: File.open("db/images/property_9.png"), filename: "property_9.png")
-  property.images.attach(io: File.open("db/images/property_10.png"), filename: "property_10.png")
-  property.images.attach(io: File.open("db/images/property_11.png"), filename: "property_11.png")
-  property.images.attach(io: File.open("db/images/property_12.png"), filename: "property_12.png")
+  property.images.attach(io: File.open("db/images/property_#{i + 1}.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_7.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_8.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_9.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_10.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_11.png"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_12.png"), filename: property.name)
 
   amenity_set = Set.new
   ((10..(amenities_data.length() - 1)).to_a.sample).times do
@@ -144,7 +125,7 @@ end
       location_rating: (1..5).to_a.sample,
       value_rating: (1..5).to_a.sample,
       property: property,
-      user: User.all.sample
+      user: User.all.sample,
     })
   end
 end
